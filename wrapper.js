b10a57f20f2e49d0104c7eb06c13e77b4b2d1bc9
smhasanmansur <smhasanmansur@gmail.com>
2016-04-19 12:40:36 +0600

    bulk string & array creator

--- wrapper.js
+++ wrapper.js
@@ -4,11 +4,22 @@ function wrapper () {
 }
 
 wrapper.prototype.wrap = function (command, key, val, cb) {
-    var req = message.STAR + "3" + message.CRLF
-              + message.DOLLAR + command.length + message.CRLF + command + message.CRLF
-              + message.DOLLAR + key.length + message.CRLF + key + message.CRLF
-              + message.DOLLAR + val.length + message.CRLF + val + message.CRLF;
-    cb(req);
+    this.createArray(command, key, val, function (req) {
+        cb(req);
+    });
+}
+
+wrapper.prototype.createArray = function () {
+    var req = "";
+    for (var i=0; i<arguments.length-1; i++) {
+        req += this.createBulkString(arguments[i]); 
+    }
+    req = message.STAR + (arguments.length-1).toString() + message.CRLF + req;
+    arguments[arguments.length-1](req);
+}
+
+wrapper.prototype.createBulkString = function (str) {
+    return message.DOLLAR + str.length + message.CRLF + str + message.CRLF
 }
 
 module.exports = new wrapper();
