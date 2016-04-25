e19d797416969c3b9f4751126518a1dfa564c4bd
smhasanmansur <smhasanmansur@gmail.com>
2016-04-25 14:49:58 +0600

    command name capitalized

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var host = "localhost";
 var port = 6379;
 
 resp.createClient(port, host);
-resp.set("name", "mansur");
-resp.get("name");
-resp.getRange("name", ["0", "2"]);
+resp.SET("name", "mansur");
+resp.GET("name");
+resp.GETRANGE("name", ["0", "2"]);
 resp.close();
